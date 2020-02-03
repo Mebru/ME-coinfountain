82696cebf39fac82a135b47e26c619cdc3f3b840
Wouter <wtlangerman@gmail.com>
2020-02-03 06:57:31 +0200

    Basic Coin Fountain Colission
    Disabling collideWorldBounds.

--- src/states/Boot.ts
+++ src/states/Boot.ts
@@ -1,29 +1,25 @@
 import { UIKeyValues, UIKeys } from "/assets"
 
-export class Boot extends Phaser.State
-{
+export class Boot extends Phaser.State {
     public static key = "Boot"
     public static onCreate = new Phaser.Signal()
 
     public key = Boot.key
 
-    init()
-    {
+    init() {
         console.log("State", this.key)
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
-        this.scale.setMinMax(320, 240, 1280, 960)
+        this.scale.setMinMax(320, 240, 1920, 1080)
         // Disable generic right click menu.
         this.game.canvas.addEventListener('contextmenu', (event) => event.preventDefault())
         this.game.stage.disableVisibilityChange = true
     }
 
-    preload()
-    {
+    preload() {
         this.load.image(UIKeys.preloader_bar, UIKeyValues.preloader_bar)
     }
 
-    create()
-    {
+    create() {
         Boot.onCreate.dispatch()
     }
 }
